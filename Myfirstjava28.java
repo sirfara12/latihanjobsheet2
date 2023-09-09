c946f9a43cd8625b9a5f1cd5fc8949012d4eb56a
sirfara <you@msirvaratigmail.com>
2023-09-09 17:05:48 +0700

    latihan jobsheet 2

---
+++ Myfirstjava28.java
@@ -0,0 +1,14 @@
+public class Myfirstjava28 {
+    public static void main(String[] args){
+        System.out.print("Hallo nama saya Sirfaratih ");
+        System.out.print("saya adalah mahasiswa baru jurusan teknologi informasi-polinema");
+        System.out.print("\n saya adalah mahasiswa jurusan teknologi informasi-POLINEMA");
+
+    System.out.println("*****IDENTITAS DIRI*****");
+    System.out.println("NAMA     : SIRFARATIH");
+    System.out.println("ABSEN    : 28");
+    System.out.println("KELAS    : TI 1A");
+    System.out.println("PRODI    : D4 TEKNIK INFORMATIKA");
+    System.out.println("JURUSAN  : TEKNOLOGI INFORMASI");
+    }
+}
